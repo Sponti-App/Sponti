51509de48fda3c9c25aac86abe4ceac995d728eb
NilAngelats <nilangelatsregincos@gmail.com>
2026-05-08 16:56:53 +0200

    Solving export

--- spa/next.config.mjs
+++ spa/next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true, // required for static export
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
