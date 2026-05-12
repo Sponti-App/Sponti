29a57a11a0b586e118c7d45eb3a44be7676e88b3
Samara <kyzy.samara@gmail.com>
2026-05-12 15:51:59 +0200

    uploadAvatar backend part

--- auth-server/src/middleware/index.ts
+++ auth-server/src/middleware/index.ts
@@ -2,3 +2,4 @@ export { default as validateBody } from './validateBody.ts';
 export { default as errorHandler } from "./errorHandler.ts";
 export { default as notFoundHandler } from "./notFoundHandler.ts";
 export { default as requireAuth } from "./requireAuth.ts";
+export { uploadAvatar } from "./uploadAvatar.ts";
--- auth-server/src/routes/authRoutes.ts
+++ auth-server/src/routes/authRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { login, logout, me, register, refresh, forgotPassword, resetPassword } from "#controllers";
-import { requireAuth, validateBody } from "#middleware";
+import { login, logout, me, register, refresh, forgotPassword, resetPassword, updateAvatar } from "#controllers";
+import { requireAuth, validateBody, uploadAvatar } from "#middleware";
 import { loginSchema, registerSchema, refreshTokenSchema, logoutSchema, forgotPasswordSchema, resetPasswordSchema } from "#schemas";
 
 const router = Router();
@@ -12,5 +12,10 @@ router.post("/logout", requireAuth, validateBody(logoutSchema), logout);
 router.get("/me", requireAuth, me);
 router.post("/forgot-password", validateBody(forgotPasswordSchema), forgotPassword);
 router.post("/reset-password", validateBody(resetPasswordSchema), resetPassword);
+router.patch("/me/avatar",
+    requireAuth,
+    uploadAvatar.single("avatar"),
+    updateAvatar
+);
 
 export default router;
