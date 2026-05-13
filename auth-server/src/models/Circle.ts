20c2b4894bd5c3fe3339104f9e1093293954e22a
NilAngelats <nilangelatsregincos@gmail.com>
2026-05-13 08:33:10 +0200

    Adding type in circle creation and TS

--- auth-server/src/models/Circle.ts
+++ auth-server/src/models/Circle.ts
@@ -1,25 +1,43 @@
-import { model, Schema } from "mongoose";
+import { model, Schema, Types } from "mongoose";
 
-const circleSchema = new Schema({
+const circleTypes = ["close", "inner", "all"] as const;
+
+type CircleType = (typeof circleTypes)[number];
+
+interface CircleDocument {
+  ownerId: Types.ObjectId;
+  name: string;
+  color: string;
+  type: CircleType;
+}
+
+const circleSchema = new Schema<CircleDocument>(
+  {
     ownerId: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     name: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
     color: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
-},
-    {
-        timestamps: true,
-    }
+    type: {
+      type: String,
+      enum: circleTypes,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
 );
 
 export const Circle = model("Circle", circleSchema);
