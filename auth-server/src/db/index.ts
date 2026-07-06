878e0265aa03deaa8b0d01f8fb6afab289fa5235
NilAngelats <nilangelatsregincos@gmail.com>
2026-07-06 18:12:53 +0200

    API and auth-server pointing the same DB

--- auth-server/src/db/index.ts
+++ auth-server/src/db/index.ts
@@ -2,11 +2,24 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     const mongoURI = process.env.MONGO_URI;
+    const dbName = process.env.DB_NAME;
+
     if (!mongoURI) {
         throw new Error("MONGO_URI is not defined in environment variables", {
             cause: { status: 500 },
         });
     }
-    await mongoose.connect(mongoURI);
-    console.log("Connected to MongoDB");
+
+    if (!dbName) {
+        throw new Error("DB_NAME is not defined in environment variables", {
+            cause: { status: 500 },
+        });
+    }
+
+    await mongoose.connect(mongoURI, {
+        dbName,
+        autoIndex: process.env.NODE_ENV !== "production",
+    });
+
+    console.log(`Connected to MongoDB database "${dbName}"`);
 };
